d42935358ce45dd287b4aec5796580a81e2bb86e
Kenyo <kenyo@MacBook-Air-Kenyo.local>
2021-11-24 02:00:25 +0300

    feat: di with spring annotation config

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,12 +2,13 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
-// Stub
 public interface PostRepository {
   public List<Post> all();
   public Optional<Post> getById(long id);
   public Post save(Post post);
   public void removeById(long id);
+
 }
