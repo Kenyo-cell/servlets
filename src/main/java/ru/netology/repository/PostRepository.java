0fca8df0fd6536455210d38c8e8cb4e5f55cf2bd
Kenyo-cell <rekurt3000@gmail.com>
2022-01-16 13:28:10 +0300

    fix: changed some inner logic near exceptions throwing

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -5,15 +5,12 @@ import ru.netology.model.Post;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  private final int NOT_FOUND = -1;
   private final int NEW_POST = 0;
 
-  private final List<Post> postsList = new CopyOnWriteArrayList<>();
   private final Map<Long, Post> posts = new ConcurrentHashMap<>();
   private final AtomicLong idCounter = new AtomicLong(0);
 
@@ -28,21 +25,17 @@ public class PostRepository {
   public Post save(Post post) throws NotFoundException {
     if (post.getId() == NEW_POST) {
       post.setId(idCounter.incrementAndGet());
-      posts.put(post.getId(), post);
-    } else {
-      if (posts.get(post.getId()) == null)
+    } else if (posts.get(post.getId()) == null)
         throw new NotFoundException("Post with id %d not found".formatted(post.getId()));
 
-      posts.put(post.getId(), post);
-    }
+    posts.put(post.getId(), post);
     return post;
   }
 
   public boolean removeById(long id) throws NotFoundException {
-    return postsList.remove(postsList.stream()
-            .filter(post -> post.getId() == id)
-            .findFirst()
-            .orElseThrow(NotFoundException::new)
-    );
+    if (posts.get(id) == null)
+      throw new NotFoundException("Post with id %d not found".formatted(id));
+    posts.remove(id);
+    return true;
   }
 }
